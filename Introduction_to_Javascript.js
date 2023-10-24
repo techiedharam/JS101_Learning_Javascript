1c2713d7ab6f1641c4217e3cac5d059fd71ee9ba
Dharmender <dharmenderarya477@gmail.com>
2023-10-25 00:32:18 +0530

    Introduction to Javascript at Masai

---
+++ Introduction_to_Javascript.js
@@ -0,0 +1,62 @@
+// JavaScript Variable
+// A JavaScript variable is simply a name of storage location. There are two types of variables in JavaScript :
+// local variable and global variable.
+// 1. Name must start with a letter (a to z or A to Z), underscore( _ ), or dollar( $ ) sign.
+// 2. After first letter we can use digits (0 to 9), for example value1.
+// 3. JavaScript variables are case sensitive, for example x and X are different variables.
+var name = "Masai School";
+console.log(name);
+
+var a = 20;
+console.log(a);
+// Assignment 1
+console.log("Masai School");
+console.log("A Transformation in Education");
+
+// Assignment 2
+var name = "Dharmender";
+console.log(name);
+var name = "Sikander Mahto";
+console.log("name");
+var name = "Usha Devi";
+console.log(name);
+
+// Assignment 3
+// Create a variable name to store your name
+var name = "Dharmender";
+// Create a variable age to store your age
+var age = "23";
+console.log(name , age);
+// Print the values stored in the variable on one line followed by the type of the variable in the next line
+
+// Assignment 4
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+
+let name = "Dharmender";
+let school = "CBSE";
+let grade = "B";
+let section = "D";
+let rollNo = 108;
+let English = 50;
+let Hindi = 70;
+let Maths = 80;
+
+console.log("          * * * * * * * * * * * * * * * * * * ");
+console.log("          *       Student Result Card        *");
+console.log("          *                                  *");
+console.log("          *  Name:   ",name ,   "            *");
+console.log("          *                                  *");
+console.log("          *  school:   ",school,"                *");
+console.log("          *                                  *");
+console.log("          *  grade:      ",grade,"                 *");
+console.log("          *                                  *");
+console.log("          *  section:    ",section ,"                 *");
+console.log("          *                                  *");
+console.log("          *  rollNo:     ",rollNo ,"               *");
+console.log("          *                                  *");
+console.log("          *  English:    ",English ,"                *");
+console.log("          *                                  *");
+console.log("          *  Hindi:      ",Hindi ,"                *");
+console.log("          *                                  *");
+console.log("          *  Maths:      ",Maths ,"                *");
+console.log("          * * * * * * * * * * * * * * * * * * ");
